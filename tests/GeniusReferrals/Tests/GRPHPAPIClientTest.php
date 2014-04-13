b31879020008daca1bc972d084e3c5fc12825f76
Alain Hernandez <alainhl@gmail.com>
2014-04-13 21:33:29 +0000

    fix directory

--- tests/GeniusReferrals/Tests/GRPHPAPIClientTest.php
+++ tests/GeniusReferrals/Tests/GRPHPAPIClientTest.php
@@ -5,7 +5,7 @@ namespace GeniusReferrals\Tests;
 /**
  * Requires
  */
-require_once __DIR__ . '/../../../../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../../../vendor/autoload.php';
 
 use GeniusReferrals\GRPHPAPIClient;
 
